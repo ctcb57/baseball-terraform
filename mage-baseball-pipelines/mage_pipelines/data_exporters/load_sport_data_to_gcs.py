622d4dd173cf96f42696136a2452fbfd652016e6
Charles Clark <charles.t.clark89@gmail.com>
2024-02-04 16:50:45 -0600

    Adding mage pipelines

---
+++ mage-baseball-pipelines/mage_pipelines/data_exporters/load_sport_data_to_gcs.py
@@ -0,0 +1,33 @@
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.config import ConfigFileLoader
+from mage_ai.io.google_cloud_storage import GoogleCloudStorage
+from pandas import DataFrame
+from os import path
+from datetime import datetime
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+
+@data_exporter
+def export_data_to_google_cloud_storage(df: DataFrame, **kwargs) -> None:
+    """
+    Template for exporting data to a Google Cloud Storage bucket.
+    Specify your configuration settings in 'io_config.yaml'.
+
+    Docs: https://docs.mage.ai/design/data-loading#googlecloudstorage
+    """
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    # with open('/home/src/gcp_secrets.json', 'r') as f:
+    #     print(f.read())
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'default'
+
+    bucket_name = 'baseball-project-charles-clark-1-dev'
+    object_key = f'sports/sport_data_{current_date}.parquet'
+
+    GoogleCloudStorage.with_config(ConfigFileLoader(config_path, config_profile)).export(
+        df,
+        bucket_name,
+        object_key,
+    )
---
+++ mage-baseball-pipelines/mage_pipelines/data_loaders/load_mlb_sport_data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import requests
+
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data(*args, **kwargs):
+    base_url = f"https://statsapi.mlb.com/api/v1/sports"
+    sport_data = requests.get(base_url).json()
+    df = pd.DataFrame(sport_data['sports'])
+    return df
+
+
+# @test
+# def test_output(output, *args) -> None:
+#     """
+#     Template code for testing the output of the block.
+#     """
+#     assert output is not None, 'The output is undefined'
