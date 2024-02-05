884a8b6d64cc14ec93cd00d85554d70498cf293c
Charles Clark <charles.t.clark89@gmail.com>
2024-02-05 00:01:11 -0600

    Adding gcs to bq integration init

--- mage-baseball-pipelines/mage_pipelines/data_loaders/load_sport_data_gcs.py
+++ mage-baseball-pipelines/mage_pipelines/data_loaders/load_sport_data_gcs.py
@@ -1,33 +1,38 @@
 from mage_ai.settings.repo import get_repo_path
 from mage_ai.io.config import ConfigFileLoader
 from mage_ai.io.google_cloud_storage import GoogleCloudStorage
-from pandas import DataFrame
 from os import path
 from datetime import datetime
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
 
-if 'data_exporter' not in globals():
-    from mage_ai.data_preparation.decorators import data_exporter
 
-
-@data_exporter
-def export_data_to_google_cloud_storage(df: DataFrame, **kwargs) -> None:
+@data_loader
+def load_from_google_cloud_storage(*args, **kwargs):
     """
-    Template for exporting data to a Google Cloud Storage bucket.
+    Template for loading data from a Google Cloud Storage bucket.
     Specify your configuration settings in 'io_config.yaml'.
 
     Docs: https://docs.mage.ai/design/data-loading#googlecloudstorage
     """
     current_date = datetime.now().strftime("%Y-%m-%d")
-    # with open('/home/src/gcp_secrets.json', 'r') as f:
-    #     print(f.read())
     config_path = path.join(get_repo_path(), 'io_config.yaml')
     config_profile = 'default'
 
     bucket_name = 'baseball-project-charles-clark-1-dev'
     object_key = f'sports/sport_data_{current_date}.parquet'
-
-    GoogleCloudStorage.with_config(ConfigFileLoader(config_path, config_profile)).export(
-        df,
+    print(object_key)
+    return GoogleCloudStorage.with_config(ConfigFileLoader(config_path, config_profile)).load(
         bucket_name,
         object_key,
     )
+
+
+# @test
+# def test_output(output, *args) -> None:
+#     """
+#     Template code for testing the output of the block.
+#     """
+#     assert output is not None, 'The output is undefined'
