20a69259970cff4c6cc99670f36d5b7b6d476b4b
Charles Clark <charles.t.clark89@gmail.com>
2024-02-10 18:43:45 -0500

    Adding schedule work

--- mage-baseball-pipelines/mage_pipelines/data_loaders/load_schedule_gcs.py
+++ mage-baseball-pipelines/mage_pipelines/data_loaders/load_schedule_gcs.py
@@ -2,7 +2,6 @@ from mage_ai.settings.repo import get_repo_path
 from mage_ai.io.config import ConfigFileLoader
 from mage_ai.io.google_cloud_storage import GoogleCloudStorage
 from os import path
-from datetime import timedelta 
 if 'data_loader' not in globals():
     from mage_ai.data_preparation.decorators import data_loader
 if 'test' not in globals():
@@ -10,25 +9,19 @@ if 'test' not in globals():
 
 
 @data_loader
-def load_from_google_cloud_storage(*args, **kwargs):
+def load_from_google_cloud_storage(object_key: str, **kwargs):
     """
     Template for loading data from a Google Cloud Storage bucket.
     Specify your configuration settings in 'io_config.yaml'.
 
     Docs: https://docs.mage.ai/design/data-loading#googlecloudstorage
     """
-    previous_date = str((kwargs.get('execution_date') - timedelta(days=1)).date())
-    # Remove this when ready to merge
-    previous_date = '2023-05-01'
-
     config_path = path.join(get_repo_path(), 'io_config.yaml')
     config_profile = 'default'
-
+    print(object_key)
     bucket_name = 'baseball-project-charles-clark-1-dev'
-    object_key = f'mlb/schedule/mlb_schedule_{previous_date}.parquet'
 
     return GoogleCloudStorage.with_config(ConfigFileLoader(config_path, config_profile)).load(
         bucket_name,
         object_key,
     )
-
